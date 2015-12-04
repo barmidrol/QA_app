18e1191ac5afb9466ce6cbb5a8a1126f698f7453
barmidrol <barmidrol@gmail.com>
2015-12-04 15:21:26 +0300

    Add admin logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,45 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :activate]
 
   def index
-    @users = User.all
+    if params[:approved] == "false"
+      @users = User.find_all_by_approved(false)
+    else
+      @users = User.all
+    end
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to users_path, notice: 'Success!'
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  def activate
+    if current_user.admin
+      @user.activate_account!
+      redirect_to users_path
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :approved)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
+  post "users/:id/activate", to: "users#activate", as: 'activate'
   root 'static_pages#index'
 end
